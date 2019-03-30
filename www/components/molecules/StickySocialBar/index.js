ec66723d619548d441b6fc7ee021c1180a99558d
daint2git <peterpoy93@gmail.com>
2019-03-30 18:27:54 +0700

    update social component

--- www/components/molecules/StickySocialBar/index.js
+++ www/components/molecules/StickySocialBar/index.js
@@ -1,28 +1,35 @@
 import React from 'react'
+import Proptypes from 'prop-types'
 
+import withRepeater from 'components/utils/withRepeater'
 import cssModuleNameTag from 'components/utils/cssModuleNameTag'
 
+import socialList from './socialList'
 import styles from './styles.scss'
 
 const cssModules = cssModuleNameTag(styles)
 
+const SocialItem = ({ url, name }) => (
+  <a
+    className={cssModules`social-item ${name}`}
+    href={url}
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    <i className={`fa fa-${name}`} />
+  </a>
+)
+
+SocialItem.propTypes = {
+  url: Proptypes.string,
+  name: Proptypes.string,
+}
+
+const SocialItems = withRepeater(SocialItem)
+
 const StickySocialBar = () => (
   <div className={cssModules`root`}>
-    <a href="http://facebook.com.vn" className={cssModules`facebook`}>
-      <i className="fa fa-facebook" />
-    </a>
-    <a href="https://twitter.com" className={cssModules`twitter`}>
-      <i className="fa fa-twitter" />
-    </a>
-    <a href="https://www.google.com" className={cssModules`google`}>
-      <i className="fa fa-google" />
-    </a>
-    <a href="https://www.linkedin.com" className={cssModules`linkedin`}>
-      <i className="fa fa-linkedin" />
-    </a>
-    <a href="https://www.youtube.com/" className={cssModules`youtube`}>
-      <i className="fa fa-youtube" />
-    </a>
+    <SocialItems list={socialList} />
   </div>
 )
 
---
+++ www/components/molecules/StickySocialBar/socialList.js
@@ -0,0 +1,24 @@
+const socialList = [
+  {
+    url: 'http://facebook.com',
+    name: 'facebook',
+  },
+  {
+    url: 'https://twitter.com',
+    name: 'twitter',
+  },
+  {
+    url: 'https://www.google.com',
+    name: 'google',
+  },
+  {
+    url: 'https://www.linkedin.com',
+    name: 'linkedin',
+  },
+  {
+    url: 'https://www.youtube.com',
+    name: 'youtube',
+  },
+]
+
+export default socialList
